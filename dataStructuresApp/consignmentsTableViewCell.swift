9533ab176923767b54985555d65ca51e6506c1bd
onglo <edouardlong@icloud.com>
2018-01-13 09:41:07 +0000

    working on implementing a view to view the consignments

---
+++ dataStructuresApp/consignmentsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  consignmentsTableViewCell.swift
+//  dataStructuresApp
+//
+//  Created by Edouard Long on 13/01/2018.
+//  Copyright © 2018 Edouard Long. All rights reserved.
+//
+
+import UIKit
+
+class consignmentsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mainTextBlock: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
