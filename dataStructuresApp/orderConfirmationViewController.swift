a03ec7212e2e4d7cda9718d76123be3fb6cc2d4f
onglo <edouardlong@icloud.com>
2018-01-13 09:20:58 +0000

    added entry fields for customer data

--- dataStructuresApp/orderConfirmationViewController.swift
+++ dataStructuresApp/orderConfirmationViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
+var customerData = [(name: String, address: String)]()
+
 class orderConfirmationViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextView!
+    
+    @IBOutlet weak var customerNameInput: UITextField!
+    @IBOutlet weak var customerAddressInput: UITextField!
+    
     
     // string that previous view controller will load
     var textString:String!
@@ -28,6 +34,19 @@ class orderConfirmationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func saveCustomerDataPressed(_ sender: Any) {
+        
+        // create a new customer address tuple with the data from the text entries
+        let customerTuple = (name: customerNameInput.text, address: customerAddressInput.text)
+        
+        // add customer tuple to array of customer data
+        customerData.append(customerTuple as! (name: String, address: String))
+        
+        // use a segue to go back to the main menu
+        self.performSegue(withIdentifier: "closeOrder", sender: nil)
     }
     
 
